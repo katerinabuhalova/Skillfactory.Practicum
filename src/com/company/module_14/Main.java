eb47bd6e1ed4a923c3cde58ce9daaefd1de83858
Ekaterina <katsiarina.buhalova@gmail.com>
2021-10-12 11:20:21 +0300

    Homework for module 14

--- src/com/company/module_14/Main.java
+++ src/com/company/module_14/Main.java
@@ -1,7 +1,35 @@
 package com.company.module_14;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Strings instrument with stream : ");
+        streamSolution();
+        System.out.println("Strings instrument without stream : ");
+        solutionsWithoutStream();
+    }
+
+    public static void streamSolution() {
+        List<String> stringsInstrument = Arrays.asList("Viola", "Cello", "Violin", "Piano");
+        stringsInstrument
+                .stream()
+                .filter(x -> x.toLowerCase(Locale.ROOT).startsWith("v"))
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    public static void solutionsWithoutStream() {
+        List<String> stringsInstrument = Arrays.asList("Viola", "Cello", "Violin", "Piano");
+        List<String> newStringsInstrument = new ArrayList<>();
+        for (String instrument : stringsInstrument) {
+            if (instrument.toLowerCase(Locale.ROOT).charAt(0) == 'v') {
+                newStringsInstrument.add(instrument);
+                System.out.println(instrument);
+            }
+        }
     }
 }
